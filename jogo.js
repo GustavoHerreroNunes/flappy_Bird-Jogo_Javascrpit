d00ae2b833513b1e055d8006b795aa87d75fccfe
GustavoHerreroNunes <ghnunes2010@gmail.com>
2020-09-29 16:40:29 -0300

    Aula nº3 - Efeitos, Pulo do Flappy Bird e Colisão com Chão

--- jogo.js
+++ jogo.js
@@ -1,5 +1,8 @@
 console.log('[DevSoutinho] Flappy Bird');
 
+const som_HIT = new Audio();
+som_HIT.src = './efeitos/hit.wav';
+
 const sprites = new Image();
 sprites.src = './sprites.png';
 
@@ -100,60 +103,109 @@ const messageGetReady = {
   }
 };
 
-/*Criando FlappyBird - Personagem que o player ir jogar*/
-const flappyBird = {
-  /*Coordenadas para o objeto dentro do arquivo "sprites.png"*/
-  sorceX: 0,
-  sorceY: 0,
+/*Função que indica se houve colisão do Flappy Bird com o Chão*/
+function fazColisao(flappyBird, chao){
+  const flappyBirdY = flappyBird.coordY + flappyBird.itemHeight;
+  const chaoY = chao.coordY;
 
-  /*Largura e Altura do objeto dentro do arquivo e da página*/
-  itemWidth: 34,
-  itemHeight: 24,
-
-  /*Coordenadas para o objeto dentro de "contexto"*/
-  coordX: 10,
-  coordY: 50,
-
-  /*Gravidade que o Flappy Bird sofrerá para queda, corresponde ao aumento de velocidade*/
-  gravity: 0.25,
-
-  /*Velocidade de queda do Flappy Bird*/
-  speed: 0,
-
-  /*Função que atualiza a posição do objeto*/
-  atualiza(){
-    this.speed = this.speed + this.gravity;
-    this.coordY = this.coordY + this.speed;
-  },
-
-  /*Função para desenhar o objeto*/
-  desenha(){
-    contexto.drawImage(
-      sprites,
-      this.sorceX, this.sorceY,
-      this.itemWidth, this.itemHeight,
-      this.coordX, this.coordY,
-      this.itemWidth, this.itemHeight
-    );
+  if(flappyBirdY >= chaoY){
+    return true;
   }
+
+  return false;
+}
+
+/*Criando FlappyBird - Personagem que o player ir jogar*/
+function criarFlappyBird(){
+  
+  const flappyBird = {
+    /*Coordenadas para o objeto dentro do arquivo "sprites.png"*/
+    sorceX: 0,
+    sorceY: 0,
+
+    /*Largura e Altura do objeto dentro do arquivo e da página*/
+    itemWidth: 34,
+    itemHeight: 24,
+
+    /*Coordenadas para o objeto dentro de "contexto"*/
+    coordX: 10,
+    coordY: 50,
+
+    /*Gravidade que o Flappy Bird sofrerá para queda, corresponde ao aumento de velocidade*/
+    gravity: 0.25,
+
+    /*Velocidade de queda do Flappy Bird*/
+    speed: 0,
+
+    /*Pulo do Flappy Bird*/
+    pulo: 4.6,
+
+    /*Função que faz o Flappy Bird pular*/
+    pula(){
+      console.log('==============');
+      console.log('[antes]', this.speed);
+
+      this.speed = - (this.pulo);
+
+      console.log('[depois]', this.speed);
+    },
+
+    /*Função que atualiza a posição do objeto*/
+    atualiza(){
+      if(fazColisao(flappyBird, chao)){
+        console.log('Game Over');
+
+        som_HIT.play();
+
+        setTimeout(() => { mudarTela(telas.INICIO); }, 500);//Esperando 1/2 segundo para mudar para a tela de Início
+
+        return;
+      }
+
+      this.speed = this.speed + this.gravity;
+      this.coordY = this.coordY + this.speed;
+    },
+
+    /*Função para desenhar o objeto*/
+    desenha(){
+      contexto.drawImage(
+        sprites,
+        this.sorceX, this.sorceY,
+        this.itemWidth, this.itemHeight,
+        this.coordX, this.coordY,
+        this.itemWidth, this.itemHeight
+      );
+    }
+  }
+  return flappyBird;
 };
+
+const globais = {};//Criando objeto para armazenar objetos do jogo que serão utilizados por várias telas
 
 let telaAtiva = {};//Criando objeto para armazenar tela ativa do jogo
 
 /*Função para mudar a tela ativa do jogo*/
 function mudarTela(novaTela){
   telaAtiva = novaTela;
+
+  if(telaAtiva.inicializa){
+    telaAtiva.inicializa();
+  }
 };
 
 /*Criando objeto para armazenar todas as telas do jogo*/
 const telas = {
   /*Tela de Início*/
   INICIO:{
+    /*Inicializa objeto global flappyBird*/
+    inicializa(){
+      globais.flappyBird = criarFlappyBird();
+    },
     /*Desenha todos os elementos da tela*/
     desenha(){
       cenario.desenha();
       chao.desenha();
-      flappyBird.desenha();
+      globais.flappyBird.desenha();
     
       messageGetReady.desenha();
     },
@@ -172,11 +224,15 @@ telas.JOGO = {
   desenha(){
     cenario.desenha();
     chao.desenha();
-    flappyBird.desenha();
+    globais.flappyBird.desenha();
   },
   /*Atualiza os elementos da tela*/
   atualiza(){
-    flappyBird.atualiza();
+    globais.flappyBird.atualiza();
+  }
+  ,
+  click(){
+    globais.flappyBird.pula();
   }
 };
 
